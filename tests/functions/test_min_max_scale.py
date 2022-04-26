1a71b3a6efa358188f5389f42a024ef8d3dced4a
Zero <Zeroto521@gmail.com>
2022-04-26 16:11:06 +0800

    TST: test the new coming option

--- tests/functions/test_min_max_scale.py
+++ tests/functions/test_min_max_scale.py
@@ -25,3 +25,41 @@ def test_min_max_old_min_max_errors(dataframe):
 def test_min_max_new_min_max_errors(dataframe):
     with pytest.raises(ValueError):
         dataframe.min_max_scale(column_name="a", new_min=10, new_max=0)
+
+
+@pytest.mark.parametrize(
+    "df, excepted, entire_data, column_name",
+    [
+        (
+            pd.DataFrame({"a": [1, 2], "b": [0, 1]}),
+            pd.DataFrame({"a": [0.5, 1], "b": [0, 0.5]}),
+            True,
+            None,
+        ),
+        (
+            pd.DataFrame({"a": [1, 2], "b": [0, 1]}),
+            pd.DataFrame({"a": [0, 1], "b": [0, 1]}),
+            False,
+            None,
+        ),
+        (
+            pd.DataFrame({"a": [1, 2], "b": [0, 1], c: [0, 0]}),
+            pd.DataFrame({"a": [0.5, 1], "b": [0, 0.5]}),
+            True,
+            ["a", "b"],
+        ),
+        (
+            pd.DataFrame({"a": [1, 2], "b": [0, 1], c: [0, 0]}),
+            pd.DataFrame({"a": [0, 1], "b": [0, 1]}),
+            False,
+            ["a", "b"],
+        ),
+    ],
+)
+def test_min_max_scale_entire_data(df, excepted, entire_data, column_name):
+    result = df.min_max_scale(
+        entire_data=entire_data,
+        column_name=column_name,
+    )
+
+    assert result.equals(excepted)
