0cf3d75ff74258bc039a11bd7641ca3561c35eb6
Zero <Zeroto521@gmail.com>
2022-11-21 13:40:41 +0800

    Add annotations

--- janitor/functions/utils.py
+++ janitor/functions/utils.py
@@ -1,4 +1,7 @@
 """Utility functions for all of the functions submodule."""
+
+from __future__ import annotations
+
 import fnmatch
 import warnings
 from collections.abc import Callable as dispatch_callable
